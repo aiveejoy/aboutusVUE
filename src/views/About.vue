e2507d19793e98d3b15596bf513d81f52ae535cd
Ivy Joy Devilleres <devilleresiv_sd2023@ceaccs.net>
2019-10-29 16:32:37 +0800

    e edit ni mem

---
+++ src/views/About.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="about">
+    <h1>About the Developers</h1>
+    <developers></developers>
+  </div>
+</template>
+
+<script>
+import devs from '@/components/Dev'
+export default {
+  components: {
+    'developers': devs
+  }
+}
+</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="@/assets/toothlogo.png">
+    <HelloWorld msg="ToothBook"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'home',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+img {
+  height: 500px;
+  width: 500px;
+}
+</style>
